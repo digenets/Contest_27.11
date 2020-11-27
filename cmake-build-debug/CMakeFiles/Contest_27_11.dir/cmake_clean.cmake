68839a38901e12a5d8ca56d0b70a80d4b1d8a565
datsky-prince <digenets@gmail.com>
2020-11-27 09:40:47 +0300

    Start version

---
+++ cmake-build-debug/CMakeFiles/Contest_27_11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Contest_27_11.dir/main.c.o"
+  "Contest_27_11"
+  "Contest_27_11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Contest_27_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

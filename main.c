3cf7c16e825850d705883ba2744bc69f1c43f372
datsky-prince <digenets@gmail.com>
2020-11-27 13:16:10 +0300

    VER2

--- main.c
+++ main.c
@@ -6,8 +6,16 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
 
 #define SIZE 100
+
+typedef struct{
+    int octade1,
+        octade2,
+        octade3,
+        octade4;
+}IP;
 
 int pow(int x, int y) {
     int temp = x;
@@ -22,14 +30,14 @@ int pow(int x, int y) {
     }
 }
 
-int count_lines1(FILE * inp){
+int count_lines(FILE * inp){
     char arr_number_of_lines[SIZE] = {0};
     int dig_num_of_lines           =  0;
     char cur                       =  0;
 
     for (int i = 0; i < SIZE; ++i){
         cur = fgetc(inp);
-        if ('\n' == cur)
+        if ('\n' == cur || '\0' == cur)
             break;
         arr_number_of_lines[i] = cur;
     }
@@ -45,21 +53,55 @@ int count_lines1(FILE * inp){
     return dig_num_of_lines;
 }
 
-int count_lines2(FILE * inp){}
+void str_skip(FILE * inp){
+    int arr[1000]= {0};
+    fgets(arr, 1000, inp);
+}
 
-typedef struct{
-    int octade1,
-        octade2,
-        octade3,
-        octade4;
-}IP;
+char* enter_ip(FILE* inp, IP str){
+    char cur         =  0;
+    char temp_arr[4] = {0};
+    char res_arr[15] = {0};
+    int  i           =  0;
+    int  lenght      =  0;
+    while (1){
+        cur = fgetc(inp);
+        if ('.' != cur)
+        {
+            temp_arr[i] = cur;
+        }
+        else
+            break;
+    }
+
+    for (int j = 0; j < strlen(temp_arr); j++){
+        str.octade1 =
+    }
+}
+
 
 int main()
 {
+    char *locale = setlocale(LC_ALL, "");
     FILE * inp = fopen("/home/datsky-prince/CLionProjects/Contest_27.11/input.txt", "rt");
     FILE * out = fopen("/home/datsky-prince/CLionProjects/Contest_27.11/output.txt", "wt");
-    int number_of_lines_1 = count_lines1(inp);
-    
+
+    int number_of_lines_1 = count_lines(inp);
+    int number_of_lines_2 = 0;
+
+    str_skip(inp);
+    for (int i = 0; i < number_of_lines_1; ++i){
+        str_skip(inp);
+    }
+
+    number_of_lines_2 = count_lines(inp);
+    IP adress;
+    IP mask;
+    char
+
+
+
+
 
 
     return 0;
